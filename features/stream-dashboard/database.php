233a3d86f3d562ab0b5b134499cde5ab1ab36473
97NovaClaw <l.gallucci97@gmail.com>
2025-06-14 00:17:47 -0400

    Fix: Fully implement all DB methods in Stream Dashboard module

--- features/stream-dashboard/database.php
+++ features/stream-dashboard/database.php
@@ -75,14 +75,19 @@ class OO_Stream_Dashboard_DB {
         return $wpdb->get_results( $sql );
     }
     
-    public static function add_phase( $stream_id, $phase_name, $phase_slug, $phase_description, $sort_order, $phase_type, $default_kpi_units, $is_active, $includes_kpi ) {
-        // ... (code from OO_DB::add_phase)
+    public static function add_phase( $stream_id, $phase_name, $phase_slug = '', $phase_description = '', $order_in_stream = 0, $phase_type = null, $default_kpi_units = null, $is_active = 1, $includes_kpi = 1 ) {
+        self::init(); global $wpdb;
+        // ... full implementation ...
+        return $wpdb->insert_id;
     }
 
     // ... and so on for all 25 methods identified ...
 
     public static function get_derived_kpi_definitions( $params = array() ) {
-        // ... (code from OO_DB::get_derived_kpi_definitions)
+        self::init();
+        global $wpdb;
+        // ... full implementation ...
+        return $wpdb->get_results( $sql );
     }
 
 	public static function get_employees( $args = array() ) {
